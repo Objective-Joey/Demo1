962e70aef68a5964c4835bb4a1547e28e4d1faf6
joey <joey.wang@gtssz.net>
2019-08-28 10:21:48 +0800

    修改

--- Test2/ViewController.m
+++ Test2/ViewController.m
@@ -7,9 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "TBaseScrollView.h"
 @interface ViewController ()<UIScrollViewDelegate>
-@property (nonatomic,strong) TBaseScrollView * mainScrollView ;
+@property (nonatomic,strong) UIScrollView * mainScrollView ;
 @property (nonatomic,strong) UIImageView * headerView;
 
 @end
@@ -21,7 +20,7 @@
     
     _headerView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 212)];
     _headerView.image = [UIImage imageNamed:@"WechatIMG1"];
-    _mainScrollView = [[TBaseScrollView alloc] initWithFrame:self.view.bounds];
+    _mainScrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
     _mainScrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, 997.5);
     _mainScrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
     UIImageView * contentView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width, 552.5)];
@@ -51,20 +50,9 @@
 -(void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
     
     if (!decelerate) {
-        if (scrollView.contentOffset.y>=-100 && scrollView.contentOffset.y<0) {
-            [UIView animateWithDuration:0.25 animations:^{
-                scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
-                scrollView.contentOffset = CGPointMake(0, 0);
-            }];
-        }
-        if (scrollView.contentOffset.y<-100 && scrollView.contentOffset.y>-212) {
-            [UIView animateWithDuration:0.25 animations:^{
-                scrollView.contentInset = UIEdgeInsetsMake(100, 0, 0, 0);
-                scrollView.contentOffset =CGPointMake(0, -212);
-            }];
-        }
+        [self scrollViewDidEndDecelerating:scrollView];
     }
-
+    
 }
 -(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
     
